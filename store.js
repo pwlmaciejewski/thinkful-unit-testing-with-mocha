67b1e44a3d3f80dc44847cfc28ef41b5e496cdc2
Paweł Maciejewski <pwlmaciejewski@gmail.com>
2015-10-28 03:55:35 -0400

    After Part 1

--- store.js
+++ store.js
@@ -1,5 +1,13 @@
-function Store() {
+function Store(initialState) {
+  this.state = initialState || {};
+}
 
+Store.prototype.getState = function() {
+  return this.state;
+}
+
+Store.prototype.get = function(key) {
+  return this.state[key];
 }
 
 module.exports = Store;
--- test.js
+++ test.js
@@ -2,8 +2,41 @@ var Store = require('./store');
 var assert = require('assert');
 
 describe('Store', function() {
+  describe('Initial state', function() {
 
-  it('Does nothing', function() {
+    it('Takes initial state', function() {
+      var initialState = {
+        foo: 'bar'
+      };
+      var store = new Store(initialState);
+      assert.deepEqual(store.getState(), {
+        foo: 'bar'
+      });
+    });
+
+    it('Creates an empty state', function() {
+      var store = new Store();
+      assert.deepEqual(store.getState(), {});
+    });
+
+  });
+
+  describe('get()', function() {
+    before(function() {
+      this.store = new Store({
+        foo: 'bar'
+      });
+    });
+
+    it('Return attribute value', function() {
+      assert.equal(this.store.get('foo'), 'bar');
+    });
+
+    it('Does not throw error on non-existent key', function() {
+      assert.doesNotThrow(function() {
+        this.store.get('xxx');
+      }.bind(this));
+    });
 
   });
 
